6d6cbcad635fe93fdc89fff7dc7b86d9a79bed22
tallessalmon <54995004+tallessalmon@users.noreply.github.com>
2024-03-04 11:51:09 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
