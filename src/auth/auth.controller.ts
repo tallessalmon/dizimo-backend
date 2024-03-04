62c715e847b1c6494dcb80936f73c74810512003
tallessalmon <54995004+tallessalmon@users.noreply.github.com>
2024-03-04 12:56:11 -0300

    fix/login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,11 +10,17 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
