dbc5ee26bc5978a13081d1345eb6fe2fbb5ab7e5
Talles Salmon <tallessalmon@hotmail.com>
2024-09-04 19:47:15 -0300

    add: chrome-aws-lambda

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { Client, LocalAuth } from 'whatsapp-web.js';
 import * as qrcode from 'qrcode-terminal';
+import chromium from 'chrome-aws-lambda';
 
 @Module({
   providers: [{
@@ -9,15 +10,19 @@ import * as qrcode from 'qrcode-terminal';
       const client = new Client({
         authStrategy: new LocalAuth(),
         puppeteer: {
-          args: ['--no-sandbox', '--disable-setuid-sandbox']
+          args: ['--no-sandbox', '--disable-setuid-sandbox'],
+          executablePath: await chromium.executablePath,
         }
       });
+
       client.on('qr', qr => {
         qrcode.generate(qr, { small: true });
       });
+
       client.on('ready', () => {
         console.log('Client is ready!');
       });
+
       await client.initialize();
       return client;
     }
