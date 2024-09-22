d87c92076a785a530cd154af0066920243ba1ee8
Talles Salmon <tallessalmon@hotmail.com>
2024-09-22 14:04:50 -0300

    tasks add

---
+++ prisma/migrations/20240922170408_all_migration/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `Informations` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `abrangence` VARCHAR(191) NOT NULL,
+    `message` TEXT NOT NULL,
+    `image` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Informations` ADD CONSTRAINT `Informations_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
